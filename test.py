61b1e8bae710d2778ecf068d3266c56eb1686b80
杨康 <yangkang@ncamc.com.cn>
2023-03-29 11:23:19 +0800

    增加test实例

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import jqktrader.api
+from jqktrader.utils.stock import get_today_ipo_data
 
 
 if __name__ == "__main__":
@@ -11,9 +12,24 @@ if __name__ == "__main__":
 
   # 资金情况
   print(user.balance)
-  # 持仓情况
+  # # 持仓情况
   print(user.position)
   # 买入
-  user.buy('162411', price=0.55, amount=100)
-  #卖出
-  user.sell('162411', price=3.55, amount=100)
+  buymessage = user.buy('162411', price=0.7, amount=100)
+#   # #卖出
+#   # sellmessage = user.sell('162411', price=3.55, amount=100)
+# #   一键打新
+#   user.auto_ipo()
+#   user.cancel_entrust(buymessage['entrust_no'])
+#   # 查询当日成交
+#   # user.today_trades
+#   # 查询当日委托
+#   entrusts = user.today_entrusts
+#   print(entrusts)
+#   # 查询当日可打新股
+#   ipo_data = get_today_ipo_data()
+#   print(ipo_data)
+#   # 刷新数据
+#   user.refresh()
+#   # 退出客户端
+#   user.exit()
